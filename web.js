da6769827275cbfc708087545ca4d709b2935046
fred wang <wang.yuanhong73@gmail.com>
2014-02-14 01:25:42 -0800

    first commit

---
+++ web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var buf = fs.readFileSync('index.html');
+  //var buf = new Buffer(16);
+  //buf.toString(page);
+  response.send(buf.toString('utf8'));
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
